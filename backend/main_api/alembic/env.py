e5d9088ac8e21d5760c493666d9001c532f939a1
Ivan Isakov <isakov.i.a1@edu.mirea.ru>
2026-06-20 16:57:38 +0300

    fix

--- backend/main_api/alembic/env.py
+++ backend/main_api/alembic/env.py
@@ -5,12 +5,17 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-
+import os
 from app.models import *
 from app.db.base import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+database_url = os.getenv("DATABASE_URL")
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not set")
+
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
